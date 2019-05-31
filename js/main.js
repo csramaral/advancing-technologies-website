d375fb84b6b992ab9bb196bd4843933921dbe3f8
César Amaral <Cesar Amaral>
2019-05-30 23:11:16 -0300

    code validation

--- js/main.js
+++ js/main.js
@@ -1,30 +1,30 @@
 
 let secretPoint = document.getElementById('easter-egg');
-console.log(secretPoint);
-
-let modalInput = document.getElementById('modal-promo');
-console.log(modalInput);
-
+let modalPromo = document.getElementById('modal-promo');
 let closeButton = document.getElementsByClassName('close')[0];
-console.log(closeButton);
-
-// secretPoint.onclick = function() {
-//     if (modalInput.className === 'modal-hidden') {
-//         modalInput.className = 'modal-shown';
-//     } else {
-//        modalInput.className = 'modal-hidden';
-//     }
-// }
+let modalInput = document.getElementById('modal-input');
+const bonusCode = 39887;
 
 // Exibe o modal ao clicar no ponto secreto
 secretPoint.addEventListener('click', () => {
-    modalInput.classList.remove('modal-hidden');
-    modalInput.classList.add('modal-shown');
+    modalPromo.classList.remove('modal-hidden');
+    modalPromo.classList.add('modal-shown');
 });
 
 // Oculta modal ao clicar no botão fechar
 closeButton.addEventListener('click', () => {
-    modalInput.classList.remove('modal-shown');
-    modalInput.classList.add('modal-hidden');
+    modalPromo.classList.remove('modal-shown');
+    modalPromo.classList.add('modal-hidden');
 });
 
+// Valida o código inserido no modal
+modalInput.addEventListener('keyup', event => {
+    const code = event.target.value;
+
+    if (code == bonusCode) {
+        alert('You won the prize! Report to Tim at the front desk with your code and receive it.');
+    } else if(code != bonusCode && event.target.value.length >= 5) {
+        alert('Sorry, but your code is not valid.');
+        modalInput.value = '';
+    }
+});
